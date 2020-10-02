1eb7f1aba9c211e5954c8b2d2d077b0831fbb5ea
burakkucukali1 <burakkucukali6@gmail.com>
2020-10-02 03:06:57 +0300

    'deployment'

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    transpileDependencies: ['vuex-persist']
+  }
+  
+  
+  module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/testCase/'
+      : '/'
+  }
